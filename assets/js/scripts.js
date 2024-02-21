5801fc9d1d40ef230c88b69ea6db0460238a823d
mohamedsobhi70 <sobhi7070@gmail.com>
2024-02-21 11:54:21 +0200

    add index

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,6 +1,21 @@
 if ($(".mobile-menu-btn").length > 0) {
-    $(".mobile-menu-btn").on("click",function(){
+    $(".mobile-menu-btn").on("click", function () {
         $(this).find("svg").toggleClass("hidden")
         $(".mobile-menu").toggleClass("show")
     })
+}
+
+
+if ($(".sources-slider").length > 0) {
+    const sliders = document.querySelectorAll(".sources-slider");
+
+    sliders.forEach(function (slider) {
+        new Swiper(slider, {
+            spaceBetween: 24,
+            slidesPerView: 'auto',
+            autoplay: true,
+            centerInsufficientSlides: true,
+            centeredSlidesBounds: true,
+        });
+    });
 }
