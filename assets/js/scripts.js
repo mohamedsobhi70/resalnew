7afc14bb3193d09185202783165bbde04d7240d4
mohamedsobhi70 <sobhi7070@gmail.com>
2024-02-22 01:59:13 +0200

    add index

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,7 +1,8 @@
 if ($(".mobile-menu-btn").length > 0) {
     $(".mobile-menu-btn").on("click", function () {
         $(this).find("svg").toggleClass("hidden")
-        $(".mobile-menu").toggleClass("show")
+        $(".mobile-menu").toggleClass("show");
+        $("body").toggleClass("overflow-hidden")
     })
 }
 
@@ -16,6 +17,37 @@ if ($(".sources-slider").length > 0) {
             autoplay: true,
             centerInsufficientSlides: true,
             centeredSlidesBounds: true,
+        });
+    });
+}
+
+
+if ($(".awards-slider").length > 0) {
+    const sliders = document.querySelectorAll(".awards-slider");
+
+    sliders.forEach(function (slider) {
+        new Swiper(slider, {
+            slidesPerView: 'auto',
+            centerInsufficientSlides: true,
+            centeredSlidesBounds: true,
+            breakpoints: {
+                0: {
+                    spaceBetween: 40,
+                    centeredSlides: true,
+                    loop: true,
+                },
+                768: {
+                    spaceBetween: 40,
+                    centeredSlides: true,
+                    loop: true,
+
+                },
+                1024: {
+                    spaceBetween: 80,
+                    centeredSlides: false,
+                    loop: false,
+                },
+            },
         });
     });
 }
